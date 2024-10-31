382155e4f5ac432de32688d7176ceb702cd34e72
imardia256 <imardia256@gmail.com>
2024-10-31 11:07:00 +0000

    testing things

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,6 +7,7 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
+    pkgs.corepack_21
     # pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
